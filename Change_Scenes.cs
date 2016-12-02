a5f426d8fadf5e83ce4da0ab182af04d13e79547
Justin Perez <jrperez525@gmail.com>
2016-12-02 15:38:07 -0500

    Added all the scripts used in the game

---
+++ Change_Scenes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Change_Scenes : MonoBehaviour {
+
+	private int i;
+
+	// Use this for initialization
+	void Start () {
+		i = SceneManager.GetActiveScene ().buildIndex;
+	}
+	
+	void OnCollisionEnter2D(Collision2D Collider) {
+		if (Collider.gameObject.tag == "Player") {
+			SceneManager.LoadScene (i + 1);
+		} else if (i == 8) {
+			SceneManager.LoadScene (0);
+		}
+	}
+}
---
+++ SlowTimeOverlayBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowTimeOverlayBehavior : SlowTimeObjBehavior {
+
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update ();
+		setAlpha (life);
+	}
+
+	public void setAlpha(float alpha) {
+		Color tmp = GetComponent<SpriteRenderer> ().color;
+		tmp.a = alpha;
+		GetComponent<SpriteRenderer> ().color = tmp;
+	}
+}
